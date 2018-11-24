f5b50a3182e7938d25154a447f721e693fbcb2e0
bighead <153405814@qq.com>
2018-11-25 00:11:09 +0800

    forward loss

--- Binary_classification.py
+++ Binary_classification.py
@@ -13,6 +13,8 @@ class logistic_regression:
     y_shape = None
     w1 = None
     b1 = None
+    y_hat = None
+    loss = None
 
     def __init__(self, x, y):
         """
@@ -45,6 +47,8 @@ class logistic_regression:
         print("y_shape: "+str(self.y_shape))
         print("w1 :"+str(self.w1.shape))
         print("b1 :"+str(self.b1))
+        print("y_hat :"+str(self.y_hat.shape))
+        print("loss :" + str(self.loss))
 
     def initial_w_b(self):
         """
@@ -58,13 +62,30 @@ class logistic_regression:
         except:
             raise AttributeError("please set layers first")
 
+    def forward(self):
+        """
+        do the forward calculate, set the self.y_hat
+        param: self.x, self.w1, self.b1
+        return: Nothing
+        """
+        self.y_hat = np.dot(self.w1, self.x) + self.b1
 
+    def loss(self):
+        """
+        do the backward calculate, set the self.loss
+        param: self.y, self.y_hat
+        return: Nothing
+        """
+        self.loss = -1 / self.x_shape[1] * np.sum((self.y * np.log(self.y_hat) + (1 - self.y) * np.log((1-self.y_hat))))
 
 if __name__ == "__main__":
     x = np.ones((10, 300))
-    y = np.ones((1, 10))
+    y = np.ones((1, 300))
     log_reg = logistic_regression(x, y)
     log_reg.set_layers(10)
     log_reg.initial_w_b()
+    print("forward")
+    log_reg.forward()
+    log_reg.loss()
     log_reg.print_all()
 
