c9d4e56d6d357679439da0325b0e078342f5484d
bighead <153405814@qq.com>
2018-11-25 16:01:06 +0800

    add the gradient decent

--- Binary_classification.py
+++ Binary_classification.py
@@ -4,6 +4,14 @@
 
 import numpy as np
 from util import load_training_data
+def loss(x_shape, y, y_hat, ):
+    """
+    do the backward calculate, set the self.loss
+    param: self.y, self.y_hat
+    return: Nothing
+    """
+    return -1 / x_shape[1] * np.sum((y * np.log(y_hat+0.0000001) + (1 - y) * np.log((1 - y_hat)+0.00000001)))
+
 
 class logistic_regression:
 
@@ -16,6 +24,9 @@ class logistic_regression:
     b1 = None
     y_hat = None
     loss = None
+    dw = None
+    db = None
+    dJ = None
 
     def __init__(self, x, y):
         """
@@ -63,21 +74,38 @@ class logistic_regression:
         except:
             raise AttributeError("please set layers first")
 
+    def sigmoid(self, input_val):
+        """
+        sigmoid function
+        param: input_val
+        return: sigmoid(input_val)
+        """
+        return 1 / (1 + np.exp(-input_val))
+
     def forward(self):
         """
         do the forward calculate, set the self.y_hat
         param: self.x, self.w1, self.b1
         return: Nothing
         """
-        self.y_hat = np.dot(self.w1, self.x)
+        self.y_hat = self.sigmoid(np.dot(self.w1, self.x)+self.b1)
 
-    def loss(self):
-        """
-        do the backward calculate, set the self.loss
-        param: self.y, self.y_hat
-        return: Nothing
-        """
-        self.loss = -1 / self.x_shape[1] * np.sum((self.y * np.log(self.y_hat+0.0000001) + (1 - self.y) * np.log((1-self.y_hat)+0.00000001)))
+    def gradient_decent(self):
+        self.dw = 1/self.x_shape[1] * np.dot((self.y_hat - self.y), self.x.T)
+        self.db = 1/self.x_shape[1] * np.sum(self.y_hat - self.y)
+
+    def train(self, iters=1000, learning_rate=0.01):
+        iter_ = 0
+        while(iter_<iters):
+            iter_ += 1
+            self.forward()
+            self.loss = loss(self.x_shape, self.y, self.y_hat)
+            if iter_ % 100 == 0:
+                print("iter: " + str(iter_) + "\tloss: " + str(self.loss))
+            self.gradient_decent()
+            self.w1 = self.w1 - learning_rate * self.dw
+            self.b1 = self.b1 - learning_rate * self.db
+
 
 if __name__ == "__main__":
     x, y = load_training_data() 
@@ -85,8 +113,4 @@ if __name__ == "__main__":
     log_reg = logistic_regression(x, y)
     log_reg.set_layers(10)
     log_reg.initial_w_b()
-    print("forward")
-    log_reg.forward()
-    log_reg.loss()
-    log_reg.print_all()
-
+    log_reg.train()
