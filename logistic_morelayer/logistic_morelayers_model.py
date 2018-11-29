2f2662318b40359478b2dc2bf33eecc5c7b3df0a
bighead <153405814@qq.com>
2018-11-29 22:55:49 +0800

    just save my change

--- logistic_morelayer/logistic_morelayers_model.py
+++ logistic_morelayer/logistic_morelayers_model.py
@@ -12,6 +12,9 @@ class logistic_regression_more:
     y_shape = None
     x = None
     y = None
+    hidden_layers = None
+    w = None
+    b = None
 
     def __init__(self, x, y):
         """
@@ -32,9 +35,31 @@ class logistic_regression_more:
         """
         sigmoid = 1 / (1 + np.exp(-z))
         return sigmoid
+    
+    def initial_param(self, hidden_layers=3):
+        """
+        initial the param w, b considering with the hidder_layers' numbers
+        param: hidden_layers
+        return: Nothing
+        """
+        self.w = np.random.randn(self.x_shape[0], hidden_layers)
+        self.b = np.random.randn()
+        print("initial param successfully!")
+
+    def train(self, hidden_layers=3, iters=1):
+        self.hidden_layers = hidden_layers
+        self.initial_param()
+        i = 0
+        while(i<iters):
+            print(self.w.shape)
+            print(self.x_shape)
+            z = np.dot(self.w.T, self.x) + self.b
+            a = self.sigmoid(z)
+            i+=1
+        print(a.shape)
+
 
 if __name__ == "__main__":
     x, y = load_training_data()
     log_reg_more = logistic_regression_more(x, y)
-    sigmoid = log_reg_more.sigmoid(x)
-    print(sigmoid.shape)
+    log_reg_more.train()
